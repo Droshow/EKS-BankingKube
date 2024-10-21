fe16a0a3957f4a44ccfd8d9a270a2b7181e53f40
Droshow <mojowateam@gmail.com>
2024-10-21 17:05:03 +0200

    [21.10.2024 Kube_app] Reformat check_capabilities & privilege to yaml structure

--- BankingKube_app/Kube_Police/pkg/admission/context_capabilities/check_capabilities.go
+++ BankingKube_app/Kube_Police/pkg/admission/context_capabilities/check_capabilities.go
@@ -2,10 +2,19 @@ package context_capabilities
 
 import (
 	"encoding/json"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
 	admissionv1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
-	"log"
 )
+
+// Capabilities defines a structure for capabilities policies
+type Capabilities struct {
+	AllowedCapabilities    []string `yaml:"allowedCapabilities"`
+	DisallowedCapabilities []string `yaml:"disallowedCapabilities"`
+}
 
 // CheckCapabilities ensures that the pod does not use dangerous Linux capabilities
 func CheckCapabilities(request *admissionv1.AdmissionRequest) bool {
@@ -17,30 +26,59 @@ func CheckCapabilities(request *admissionv1.AdmissionRequest) bool {
 		return false // Fails the validation if the pod can't be parsed
 	}
 
+	// Retrieve the capabilities policies
+	capabilities, err := getCapabilities()
+	if err != nil {
+		log.Println("Failed to load capabilities policies:", err)
+		return false
+	}
+
 	// Check capabilities for each container
 	for _, container := range pod.Spec.Containers {
 		if container.SecurityContext != nil && container.SecurityContext.Capabilities != nil {
 			for _, cap := range container.SecurityContext.Capabilities.Add {
-				if IsDangerousCapability(string(cap)) {
-					log.Println("Dangerous capability found in container:", container.Name, "Capability:", cap)
-					return false // Return false if any dangerous capability is found
+				if isDisallowedCapability(string(cap), capabilities.DisallowedCapabilities) {
+					log.Println("Disallowed capability found in container:", container.Name, "Capability:", cap)
+					return false // Return false if any disallowed capability is found
 				}
 			}
 		}
 	}
 
-	// Passes the check if no dangerous capabilities are found
+	// Passes the check if no disallowed capabilities are found
 	return true
 }
 
-// IsDangerousCapability checks if a capability is considered dangerous
-func IsDangerousCapability(cap string) bool {
-	dangerousCapabilities := map[string]bool{
-		"CAP_SYS_ADMIN":  true,
-		"CAP_NET_ADMIN":  true,
-		"CAP_SYS_MODULE": true,
-		"CAP_SYS_PTRACE": true,
+// isDisallowedCapability checks if a capability is in the list of disallowed capabilities
+func isDisallowedCapability(cap string, disallowedCapabilities []string) bool {
+	for _, disallowedCap := range disallowedCapabilities {
+		if cap == disallowedCap {
+			return true
+		}
+	}
+	return false
+}
+
+// getCapabilities loads the capabilities policies from the configuration file
+func getCapabilities() (*Capabilities, error) {
+	configPath := os.Getenv("SECURITY_POLICIES_PATH")
+	if configPath == "" {
+		configPath = "configs/security-policies.yaml" // Default path
 	}
 
-	return dangerousCapabilities[cap]
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var policies struct {
+		Capabilities Capabilities `yaml:"capabilities"`
+	}
+
+	err = yaml.Unmarshal(data, &policies)
+	if err != nil {
+		return nil, err
+	}
+
+	return &policies.Capabilities, nil
 }
--- BankingKube_app/Kube_Police/pkg/admission/context_capabilities/check_privilege.go
+++ BankingKube_app/Kube_Police/pkg/admission/context_capabilities/check_privilege.go
@@ -2,12 +2,21 @@ package context_capabilities
 
 import (
 	"encoding/json"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
 	admissionv1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
-	"log"
 )
 
-// checkPrivilegedContainers checks if the pod has any privileged containers or init containers
+// PodSecurityContext defines a structure for pod security context policies
+type PodSecurityContext struct {
+	AllowPrivilegeEscalation bool `yaml:"allowPrivilegeEscalation"`
+	RunAsNonRoot             bool `yaml:"runAsNonRoot"`
+}
+
+// CheckPrivilegedContainers checks if the pod has any privileged containers or init containers
 func CheckPrivilegedContainers(request *admissionv1.AdmissionRequest) bool {
 	// Parse the Pod object from the request
 	pod := &corev1.Pod{}
@@ -15,6 +24,13 @@ func CheckPrivilegedContainers(request *admissionv1.AdmissionRequest) bool {
 	if err != nil {
 		log.Println("Failed to parse pod object:", err)
 		return false // Fails the validation if the pod can't be parsed
+	}
+
+	// Retrieve the security policies for pod security context
+	podSecurityContext, err := getPodSecurityContext()
+	if err != nil {
+		log.Println("Failed to load pod security context policies:", err)
+		return false
 	}
 
 	// Check for privileged containers
@@ -25,6 +41,14 @@ func CheckPrivilegedContainers(request *admissionv1.AdmissionRequest) bool {
 		}
 		if container.SecurityContext.Privileged != nil && *container.SecurityContext.Privileged {
 			return false // Return false if any container is privileged
+		}
+		if container.SecurityContext.AllowPrivilegeEscalation != nil && *container.SecurityContext.AllowPrivilegeEscalation != podSecurityContext.AllowPrivilegeEscalation {
+			log.Println("Container", container.Name, "has AllowPrivilegeEscalation set to", *container.SecurityContext.AllowPrivilegeEscalation, "which does not match the policy")
+			return false // Return false if AllowPrivilegeEscalation does not match the policy
+		}
+		if container.SecurityContext.RunAsNonRoot != nil && *container.SecurityContext.RunAsNonRoot != podSecurityContext.RunAsNonRoot {
+			log.Println("Container", container.Name, "has RunAsNonRoot set to", *container.SecurityContext.RunAsNonRoot, "which does not match the policy")
+			return false // Return false if RunAsNonRoot does not match the policy
 		}
 	}
 
@@ -37,8 +61,40 @@ func CheckPrivilegedContainers(request *admissionv1.AdmissionRequest) bool {
 		if initContainer.SecurityContext.Privileged != nil && *initContainer.SecurityContext.Privileged {
 			return false // Return false if any init container is privileged
 		}
+		if initContainer.SecurityContext.AllowPrivilegeEscalation != nil && *initContainer.SecurityContext.AllowPrivilegeEscalation != podSecurityContext.AllowPrivilegeEscalation {
+			log.Println("Init container", initContainer.Name, "has AllowPrivilegeEscalation set to", *initContainer.SecurityContext.AllowPrivilegeEscalation, "which does not match the policy")
+			return false // Return false if AllowPrivilegeEscalation does not match the policy
+		}
+		if initContainer.SecurityContext.RunAsNonRoot != nil && *initContainer.SecurityContext.RunAsNonRoot != podSecurityContext.RunAsNonRoot {
+			log.Println("Init container", initContainer.Name, "has RunAsNonRoot set to", *initContainer.SecurityContext.RunAsNonRoot, "which does not match the policy")
+			return false // Return false if RunAsNonRoot does not match the policy
+		}
 	}
 
 	// Passes the check if no privileged containers or init containers are found
 	return true
+}
+
+// getPodSecurityContext loads the pod security context policies from the configuration file
+func getPodSecurityContext() (*PodSecurityContext, error) {
+	configPath := os.Getenv("SECURITY_POLICIES_PATH")
+	if configPath == "" {
+		configPath = "configs/security-policies.yaml" // Default path
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var policies struct {
+		PodSecurityContext PodSecurityContext `yaml:"podSecurityContext"`
+	}
+
+	err = yaml.Unmarshal(data, &policies)
+	if err != nil {
+		return nil, err
+	}
+
+	return &policies.PodSecurityContext, nil
 }
