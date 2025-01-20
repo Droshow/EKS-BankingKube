4418fd6ff9d5e72af1d1842dc0cd2db13d703d93
Droshow <mojowateam@gmail.com>
2025-01-20 13:25:48 +0100

    [20.1.2025 Infra]Weird

--- EKS_infra/modules/eks/outputs.tf
+++ EKS_infra/modules/eks/outputs.tf
@@ -3,10 +3,11 @@ output "cluster_endpoint" {
   value       = aws_eks_cluster.banking_kube_cluster.endpoint
 }
 
-output "kubeconfig_certificate_authority_data" {
-  description = "The certificate authority data for your EKS cluster"
-  value       = aws_eks_cluster.banking_kube_cluster.certificate_authority[0].data
-}
+# output "kubeconfig_certificate_authority_data" {
+#   description = "The certificate authority data for your EKS cluster"
+#   value       = aws_eks_cluster.banking_kube_cluster.certificate_authority[0].data
+# }
+
 output "cluster_name" {
   description = "The name of the EKS cluster"
   value       = var.cluster_name
