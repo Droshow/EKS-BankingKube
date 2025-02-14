67434a7f8338f01a1c7e2769a2d4b0a6a0029018
Droshow <mojowateam@gmail.com>
2025-02-14 13:45:30 +0100

    deploy

--- EKS_infra/provider.tf
+++ EKS_infra/provider.tf
@@ -8,16 +8,16 @@ provider "aws" {
     }
   }
 }
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = module.eks.cluster_certificate_authority
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args = [
-#       "eks", "get-token",
-#       "--cluster-name", module.eks.cluster_name,
-#       "--region", var.aws_region
-#     ]
-#   }
-# }
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = module.eks.cluster_certificate_authority
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks", "get-token",
+      "--cluster-name", module.eks.cluster_name,
+      "--region", var.aws_region
+    ]
+  }
+}
