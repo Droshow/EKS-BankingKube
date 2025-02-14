2269847538af91f9d6e5c6f8ff95cf68241d87d6
Droshow <mojowateam@gmail.com>
2025-02-14 14:17:58 +0100

    [14.2.2025 Infra] Security group fix typo

--- EKS_infra/main.tf
+++ EKS_infra/main.tf
@@ -3,7 +3,7 @@ module "networking" {
   alb_security_group            = [module.security.alb_sg_id]
   acm_domain_validation_options = module.security.domain_validation_options
   acm_certificate_arn           = module.security.acm_certificate_arn_existing
-  ec2_security_group_id         = 
+  ec2_security_group_id         = module.security.ec2_access_aws_security_group
 }
 
 module "security" {
