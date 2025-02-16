d3ec5e4734a9ff4a8152f90f8b2742b7efd8ae04
Droshow <mojowateam@gmail.com>
2025-02-16 03:49:54 +0100

    last try for the morning

--- EKS_infra/modules/ec2_cluster_access/main.tf
+++ EKS_infra/modules/ec2_cluster_access/main.tf
@@ -140,7 +140,7 @@ echo "GitHub Runner Token successfully retrieved."
               
                Start the GitHub Actions Runner as a service
               nohup ./run.sh > /home/ssm-user/actions-runner/runner.log 2>&1 & disown
-              # ./run.sh
+              ./run.sh
 
               EOF
 }
