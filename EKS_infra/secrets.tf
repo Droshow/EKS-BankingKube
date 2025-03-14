504a07c7d13026bf9516cbf0dd1f3095c214ac83
Droshow <mojowateam@gmail.com>
2025-03-14 08:26:45 +0100

    [14.3.2025 Deploy]

--- EKS_infra/secrets.tf
+++ EKS_infra/secrets.tf
@@ -4,7 +4,7 @@ resource "random_password" "db_password" {
 }
 
 resource "aws_secretsmanager_secret" "db_credentials" {
-  name = "bk/db_credentials-deploy-02"
+  name = "bk/db_credentials-deploy-03"
 }
 
 resource "aws_secretsmanager_secret_version" "db_credentials" {
